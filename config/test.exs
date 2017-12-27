82a9fddb39f9ec38f8c5103163ad8c8286103112
Erik Nakata <erik.nakata5@gmail.com>
2017-12-27 20:53:01 -0200

    Improvments

--- config/test.exs
+++ config/test.exs
@@ -1,15 +1,11 @@
 use Mix.Config
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :zx, ZxWeb.Endpoint,
   http: [port: 4001],
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn
 
-# Configure your database
 config :zx, Zx.Repo,
   adapter: Ecto.Adapters.Postgres,
   types: Zx.PostgresTypes,
